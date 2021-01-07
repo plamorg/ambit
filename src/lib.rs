00479b65a5c5282b92b7a368ba8c1e5edca50b88
Edward Wibowo <wibow9770@gmail.com>
2021-01-07 14:04:00 +0800

    feat: initial option implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,18 @@
 mod lexer;
+
+#[allow(unused_variables)]
+pub fn init(origin: &str) {
+    unimplemented!();
+    // TODO: implement init
+}
+
+pub fn validate() {
+    unimplemented!();
+    // TODO: implement validate
+}
+
+#[allow(unused_variables)]
+pub fn git(arguments: Vec<&str>) {
+    unimplemented!();
+    // TODO: implement git
+}
