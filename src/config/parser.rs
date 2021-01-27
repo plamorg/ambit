27d409cab7550038eb27aacb91fb72d9a11127a9
virchau13 <virchau13@mail.hexular.net>
2021-01-27 22:34:59 +0800

    refactor(config): change ParseError to encapsulate both token and ParseErrorType

--- src/config/parser.rs
+++ src/config/parser.rs
@@ -1,4 +1,4 @@
-use crate::config::{lexer::*, ParseError, ParseResult};
+use crate::config::{lexer::*, ParseError, ParseErrorType, ParseResult};
 
 use std::iter::Peekable;
 
@@ -8,7 +8,7 @@ macro_rules! expect {
             .iter()
             .find(|ty| $it.peek().map(|x| x.toktype == **ty).unwrap_or(false));
         match res {
-            None => return Err(ParseError::Expected(&$l)),
+            None => return Err(ParseError::from(ParseErrorType::Expected(&$l))),
             Some(tok) => {
                 $it.next();
                 tok
@@ -67,7 +67,10 @@ impl<I: Iterator<Item = Token>> Iterator for Parser<I> {
             Some(_) => Some({
                 let new = Entry::parse(&mut self.iter);
                 match new {
-                    Err(e) => Err((self.iter.peek().cloned(), e)),
+                    Err(mut e) => {
+                        e.tok = self.iter.peek().cloned();
+                        Err(e)
+                    },
                     Ok(p) => Ok(p),
                 }
             }),
@@ -89,14 +92,14 @@ impl Entry {
             let right_val = Spec::parse(iter)?;
             let left_nr = left
                 .nr_of_options()
-                .ok_or(ParseError::Custom("Too many options on left hand side"))?;
+                .ok_or(ParseError::from(ParseErrorType::Custom("Too many options on left hand side")))?;
             let right_nr = right_val
                 .nr_of_options()
-                .ok_or(ParseError::Custom("Too many options on right hand side"))?;
+                .ok_or(ParseError::from(ParseErrorType::Custom("Too many options on right hand side")))?;
             if left_nr != right_nr {
-                return Err(ParseError::Custom(
+                return Err(ParseError::from(ParseErrorType::Custom(
                     "Left and right sides of mapping must match up",
-                ));
+                )));
             }
             right = Some(right_val);
         }
@@ -207,7 +210,7 @@ impl Spec {
             },
         }
         if string.is_none() {
-            Err(ParseError::Expected(&[TokType::Str]))
+            Err(ParseError::from(ParseErrorType::Expected(&[TokType::Str])))
         } else {
             Ok(Spec {
                 string,
@@ -240,7 +243,7 @@ impl VariantExpr {
             .map(|x| x.toktype == TokType::RBracket)
             .unwrap_or(false)
         {
-            return Err(ParseError::Custom("Must have at least one option"));
+            return Err(ParseError::from(ParseErrorType::Custom("Must have at least one option")));
         }
         let mut specs = Vec::new();
         loop {
@@ -324,7 +327,7 @@ impl Expr {
                 }
             }
         }
-        Err(ParseError::Expected(&[TokType::Str]))
+        Err(ParseError::from(ParseErrorType::Expected(&[TokType::Str])))
     }
 }
 
@@ -359,7 +362,7 @@ mod tests {
     fn success(toks: &[Token], ast: &[Entry]) {
         let iter = toks.iter().cloned().peekable();
         match Parser::new(iter).collect::<ParseResult<Vec<_>>>() {
-            Err(e) => panic!("{:?} at token {:?}", e.1, e.0),
+            Err(e) => panic!("{:?} at token {:?}", e.ty, e.tok),
             Ok(parsed) => assert_eq!(parsed, ast),
         }
     }
@@ -368,7 +371,7 @@ mod tests {
         let res = Parser::new(iter)
             .collect::<ParseResult<Vec<_>>>()
             .unwrap_err();
-        assert_eq!(err, res.1);
+        assert_eq!(err, res);
     }
 
     #[test]
@@ -527,7 +530,7 @@ mod tests {
 
     #[test]
     fn semicolon_error() {
-        fail(&toklist!["a"], ParseError::Expected(&[TokType::Semicolon]));
+        fail(&toklist!["a"], ParseError { tok: None, ty: ParseErrorType::Expected(&[TokType::Semicolon]) });
     }
     // TODO: add more tests
 }
